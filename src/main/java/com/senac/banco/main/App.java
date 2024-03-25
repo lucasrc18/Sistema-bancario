b64669008e8a45b1262523dc351595a7a6c7499a
Lucas Rodrigues <lucas.rcaetano1122@gmail.com>
2024-03-25 16:53:32 -0300

    Javadoc and other comments

--- src/main/java/com/senac/banco/main/App.java
+++ src/main/java/com/senac/banco/main/App.java
@@ -2,6 +2,12 @@ package com.senac.banco.main;
 
 import com.senac.banco.view.GraphicalInterface;
 
+/**
+ * @author Júlia Gomes
+ * @author Lucas Rodrigues
+ *	
+ * Projeto de sistema bancario, que simula transações e operações básicas
+ * */
 public class App {
     public static void main(String[] args){
         GraphicalInterface gui = new GraphicalInterface();
--- src/main/java/com/senac/banco/view/GraphicalInterface.java
+++ src/main/java/com/senac/banco/view/GraphicalInterface.java
@@ -7,13 +7,32 @@ import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+/*
+ * Classe responsável por cordenar a interface gráfica
+ * */
 public class GraphicalInterface extends JFrame {
+	/** <code>CardLayout</code> é a classe responsável por gerenciar
+	 * o layout e exibições das páginas na janela */
 	private CardLayout layout;
+	
+	
+	/** O <code>JPanel</code> pai de toda a interface, e de todas as telas */
 	private JPanel window;
 	
+	/** O <code>JPanel</code> relacionado a página principal */
+	private JPanel main;
+	
+	/**
+	 * Constrói uma nova instancia da interface grafica
+	 * Define o tamanho da tela com um valor padrão (W 75% | H 75%)
+	 * @params null
+	 * @returns this
+	 * */
 	public GraphicalInterface() {
 		super("Sistema bancario");
 		
+		/** Define o tamanho da janela como 75% do monitor do usuario (W / H) */
+		/** Estipula as dimensões da janela baseada no tamanho da tela */
 		Dimension windowSize = Toolkit.getDefaultToolkit().getScreenSize();
 		windowSize.width = (int) Math.ceil((double) windowSize.width * 0.75);
 		windowSize.height = (int) Math.ceil((double) windowSize.height * 0.75);
@@ -22,9 +41,11 @@ public class GraphicalInterface extends JFrame {
 		this.layout = new CardLayout();
 		window.setLayout(layout);
 		
-		JPanel main = new JPanel();
+		
+		main = new JPanel();
 		window.add(main, "main");
 		
+		// Configura a janela
 		this.setVisible(true);
 		this.setPreferredSize(windowSize);
 		this.setResizable(false);
@@ -37,6 +58,12 @@ public class GraphicalInterface extends JFrame {
 		this.setLocationRelativeTo(null);
 	}
 	
+	
+	/**
+	 * Alterna entre as telas da interface
+	 * @params windowName: String
+	 * @returns void
+	 * */
 	public void goToWindow(String windowName) {
 		layout.show(window, windowName);
 	}
